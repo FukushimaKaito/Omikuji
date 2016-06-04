d683f438893ff46ea5beaaae58595f6dd5fa199d
Kaito <Kaito@VAIO-KAITO>
2016-06-05 08:08:13 +0900

    プロジェクト ファイルを追加します。

---
+++ 6.24/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _6._24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int x = rand.Next(1, 4);//1以上4未満の乱数(1～3)
+            if (x == 1)
+                label1.Text = "大吉";
+            else if (x == 2)
+                label1.Text = "吉";
+            else if (x == 3)
+                label1.Text = "凶";
+        }
+    }
+}
---
+++ 6.24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _6._24
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
